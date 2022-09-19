a8c3ce892c780273071cf0a5d6f5eda71c2c0406
johan030 <weylin030@protonmail.ch>
2022-09-19 15:28:35 +0200

    adding register route

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./components/Menu";
-
 import "./css/app.css";
 import LoginForm from "./pages/LoginForm";
+import RegisterForm from "./pages/RegisterForm";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
       </Routes>
     </>
   );

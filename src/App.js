2ef27e588e6d3de5639c4a5fd51e3c35ae5584d3
Didi518 <kevin.ouali@gmail.com>
2022-09-19 14:47:08 +0200

    setup page profil

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Menu from "./components/Menu";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './components/Menu';
 
-import "./css/app.css";
-import Login from "./pages/Login";
+import './css/app.css';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <>
       <Menu />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/connexion' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 import posts from '../posts.js';
 
 export default function Home() {
   return (
-    <div className='home-posts container mt-4'>
-      {posts.map((post) => (
-        <div key={post._id} className='posts-content'>
-          <>
-            <div className='imgBx'>
-              <img src={post.img} alt='user' />
+    <div className='row'>
+      <div className='home-posts container col-md-7 mt-4'>
+        {posts.map((post) => (
+          <Link
+            to='/profile'
+            style={{ textDecoration: 'none', color: '#5c5c5c' }}
+          >
+            <div key={post._id} className='posts-content'>
+              <>
+                <div className='imgBx'>
+                  <img src={post.img} alt='user' />
+                </div>
+                <div className='text-content'>{post.txt}</div>
+              </>
             </div>
-            <div className='text-content'>{post.txt}</div>
-          </>
-        </div>
-      ))}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }

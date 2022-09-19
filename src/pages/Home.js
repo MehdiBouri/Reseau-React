786a73e0b9a7bad0830c97694ea7c203009e5cc3
Didi518 <kevin.ouali@gmail.com>
2022-09-19 15:26:21 +0200

    clean code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Home.css';
-import posts from '../posts.js';
+import Posts from '../components/Posts';
 
 export default function Home() {
   return (
-    <div className='row'>
-      <div className='home-posts container col-md-7 mt-4'>
-        {posts.map((post) => (
-          <Link
-            to='/profile'
-            style={{ textDecoration: 'none', color: '#5c5c5c' }}
-          >
-            <div key={post._id} className='posts-content'>
-              <>
-                <div className='imgBx'>
-                  <img src={post.img} alt='user' />
-                </div>
-                <div className='text-content'>{post.txt}</div>
-              </>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <main>
+      <Posts />
+    </main>
   );
 }

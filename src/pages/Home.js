720dce99193c333d8ac7468b119e74e6022d30ca
MehdiBouri <mbouri.dev@gmail.com>
2022-09-19 09:57:25 +0200

    css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,19 @@ import posts from "../posts.js";
 
 export default function Home() {
   return (
-    <div className="home-posts container mt-4">
-      {posts.map((post) => (
-        <div key={post._id} className="posts-content">
-          <>
-            <div className="imgBx">
-              <img src={post.img} alt="user" />
-            </div>
-            <div className="text-content">{post.txt}</div>
-          </>
-        </div>
-      ))}
+    <div className="row">
+      <div className="home-posts container col-md-7 mt-4">
+        {posts.map((post) => (
+          <div key={post._id} className="posts-content">
+            <>
+              <div className="imgBx">
+                <img src={post.img} alt="user" />
+              </div>
+              <div className="text-content">{post.txt}</div>
+            </>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

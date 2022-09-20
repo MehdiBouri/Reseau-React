aeb68eb9411cf098ce2051af17c336bed6727733
johan030 <weylin030@protonmail.ch>
2022-09-20 12:19:47 +0200

    adding profile component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,56 @@
-import React from 'react';
-
-const Profile = () => {
+import React from "react";
+import posts from "../posts";
+import "../css/profile.css";
+const users = posts.filter((post) => post._id < 2);
+const Actu = () => {
   return (
-    <div className='p-container'>
-      <h2 className='p-title'>Profil</h2>
-    </div>
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="details">
+            <div className="image">
+              {users.map((user) => {
+                return (
+                  <>
+                    <img src={user.img} alt="user" />
+                  </>
+                );
+              })}
+            </div>
+            <div className="profile">
+              <h2>username</h2>
+              <div className="spec">
+                <span>Developpeuse</span>
+                <span>Pays, Région</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+
+        <div className="content">
+          <h5>Recents Posts</h5>
+          {users.map((user) => {
+            return (
+              <>
+                <p>{user.txt}</p>
+              </>
+            );
+          })}
+        </div>
+      </div>
+
+    </>
   );
 };
 
-export default Profile;
+const Recent_posts = () => { };
+export default function Profile() {
+  return (
+    <main>
+      <Actu />
+      <Recent_posts />
+    </main>
+  );
+}

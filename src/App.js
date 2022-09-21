4d2b82a40d99c423cb9faf4304b17522d30e2176
Didi518 <kevin.ouali@gmail.com>
2022-09-21 14:50:08 +0200

    correction homePage + setup url profile/id

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import "./css/app.css";
+import './css/app.css';
 
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Menu from "./components/Menu";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Profile from "./pages/Profile";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './components/Menu';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <>
       <Menu />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/connexion" element={<Login />} />
-        <Route path="/inscription" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/connexion' element={<Login />} />
+        <Route path='/inscription' element={<Register />} />
+        <Route path='/profile/:id' element={<Profile />} />
       </Routes>
     </>
   );
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import posts from "../posts";
-import "../css/home.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import posts from '../posts';
+import '../css/home.css';
 
 const Posts = () => {
   return (
     <>
       {posts.map((post) => (
-        <Link
-          to="/profile"
-          key={post._id}
-          style={{ textDecoration: "none", color: "#5c5c5c" }}
-        >
-          <div className="card">
-            <div className="row g-0">
-              <div className="col-2">
+        <div className='card'>
+          <div className='row g-0'>
+            <div className='col-2'>
+              <Link
+                to={`/profile/${post._id}`}
+                key={post._id}
+                style={{ textDecoration: 'none', color: '#5c5c5c' }}
+              >
                 <img
                   src={post.img}
-                  className="w-100 rounded-circle"
+                  className='w-100 rounded-circle'
                   alt={post.pseudo}
                 />
-              </div>
-              <div className="col-10">
-                <div className="card-body">
-                  <div className="card-infos">
-                    <h5 className="card-title">{post.pseudo}</h5>
-                    <small className="text-muted">Il y a 3 minutes</small>
-                  </div>
-                  <p className="card-text">{post.txt}</p>
+              </Link>
+            </div>
+            <div className='col-10'>
+              <div className='card-body'>
+                <div className='card-infos'>
+                  <h5 className='card-title'>{post.pseudo}</h5>
+                  <small className='text-muted'>Il y a 3 minutes</small>
                 </div>
+                <p className='card-text'>{post.txt}</p>
               </div>
             </div>
           </div>
-        </Link>
+        </div>
       ))}
     </>
   );
